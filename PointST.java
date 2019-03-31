c5d1a9aab95ba1044267bd3e8ca56c9eadc31a94
OxSon <theoxson@gmail.com>
2019-03-31 16:49:38 -0600

    Implement brute-force solution in PointST

--- PointST.java
+++ PointST.java
@@ -1,58 +1,80 @@
 package kdTree;
 
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.Queue;
+
+import java.util.TreeMap;
 
 public class PointST<Value> {
+    private TreeMap<Point2D, Value> tree;
+
     // construct an empty symbol table of points
     public PointST() {
-        //TODO
+        tree = new TreeMap<>();
     }
 
     // is the symbol table empty?
     public boolean isEmpty() {
-        //TODO
-        return false;
+        return tree.isEmpty();
     }
 
     // number of points
     public int size() {
-        //TODO
-        return 0;
+        return tree.size();
     }
 
     //associate Value val with point p
     public void put(Point2D p, Value val) {
-        //TODO
+        if(p == null || val == null)
+            throw new NullPointerException("Arguments cannot be null");
+
+        tree.put(p, val);
     }
 
     //get Value associated with point p
     public Value get(Point2D p) {
-        //TODO
-        return null;
+        if(p == null)
+            throw new NullPointerException("Arguments cannot be null");
+
+        return tree.get(p);
     }
 
     //Does the symbol table contain point p?
     public boolean contains(Point2D p) {
-        //TODO
-        return false;
+        if(p == null)
+            throw new NullPointerException("Arguments cannot be null");
+
+        return tree.containsKey(p);
     }
 
     //Iterable of all points in the symbol table
     public Iterable<Point2D> points() {
-        //TODO
-        return null;
+        return tree.navigableKeySet();
     }
 
     //all points that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
-        //TODO
-        return null;
+        var hits = new Queue<Point2D>();
+
+        for(var point : tree.keySet())
+            if (rect.contains(point))
+                hits.enqueue(point);
+
+        return hits;
     }
 
     //a nearest neighbor to point p; null if the symbol table is empty
     public Point2D nearest(Point2D p) {
-        //TODO
-        return null;
+        if(p == null)
+            throw new NullPointerException("Arguments cannot be null");
+
+        var nearest = tree.firstKey();
+
+        for(var point : tree.keySet())
+            if(p.distanceSquaredTo(point) < p.distanceSquaredTo(nearest))
+                nearest = p;
+
+        return nearest;
     }
 
     //for testing (ungraded)
