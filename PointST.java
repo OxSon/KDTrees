d616d62dcc72dac4ad8382539f83189d568b1eb1
Kenneth <TropicalDictator@gmail.com>
2019-03-31 21:46:13 -0600

    Implemented PointST.java

--- PointST.java
+++ PointST.java
@@ -1,62 +1,93 @@
 package kdTree;
 
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.RedBlackBST;
 
 public class PointST<Value> {
-    // construct an empty symbol table of points
+	RedBlackBST<Point2D, Value> table = new RedBlackBST<>();
+	
+	
+	// construct an empty symbol table of points
     public PointST() {
-        //TODO
+        table = new RedBlackBST<>();
     }
 
     // is the symbol table empty?
     public boolean isEmpty() {
-        //TODO
-        return false;
+        return table.isEmpty();
     }
 
     // number of points
     public int size() {
-        //TODO
-        return 0;
+        return table.size();
     }
 
     //associate Value val with point p
     public void put(Point2D p, Value val) {
-        //TODO
+    	table.put(p, val);
     }
 
     //get Value associated with point p
     public Value get(Point2D p) {
-        //TODO
-        return null;
+        return table.get(p);
     }
 
     //Does the symbol table contain point p?
     public boolean contains(Point2D p) {
-        //TODO
-        return false;
+        return table.contains(p);
     }
 
     //Iterable of all points in the symbol table
     public Iterable<Point2D> points() {
-        //TODO
-        return null;
+        return table.keys();
     }
 
     //all points that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
-        //TODO
-        return null;
+        Queue<Point2D> pointQueue = new Queue<>(); 
+    	for(Point2D el : points()) {
+        	if(rect.contains(el)) pointQueue.enqueue(el);
+        }
+        return pointQueue;
     }
 
     //a nearest neighbor to point p; null if the symbol table is empty
     public Point2D nearest(Point2D p) {
-        //TODO
-        return null;
+        if (table.isEmpty()) return null;
+        Point2D closest = null;
+        
+        for(Point2D el : points()) {
+        	// adds first element to list
+        	if(closest == null && !el.equals(p)) closest = el;
+        	
+        	// compares el with current closest Point2D
+        	// el distance to p is less closest to p closest get assign value of el
+        	if(!el.equals(p)) {
+        		if(p.distanceTo(el) < p.distanceTo(closest)) closest = el;
+        	}
+        }
+        
+        return closest;
     }
 
     //for testing (ungraded)
     public static void main(String[] args) {
-        //TODO
+    	PointST<String> test = new PointST<>();
+    	
+    	test.put(new Point2D(1,1), "");
+    	test.put(new Point2D(2,1), "");
+    	test.put(new Point2D(3,2), "");
+    	test.put(new Point2D(7,8), "test closest");
+    	test.put(new Point2D(7,9), "should be closest");
+    	test.put(new Point2D(11,12), "");
+    	test.put(new Point2D(3,4), "");
+    	test.put(new Point2D(4,4), "");
+    	
+    	System.out.println(test.contains(new Point2D(7,10)));
+    	System.out.println(test.get(new Point2D(7,8)));
+    	System.out.println(test.points());
+    	System.out.print(test.nearest(new Point2D(11,12)));
+    	System.out.println(test.range(new RectHV(5, 5, 12, 12)));
     }
 }
