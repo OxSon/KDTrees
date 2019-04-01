12d86f8af488e1dd9729743933c1edc41ca58d17
Kenneth <TropicalDictator@gmail.com>
2019-04-01 10:27:15 -0600

    Add basic api and node class

--- KdTreeST.java
+++ KdTreeST.java
@@ -1,4 +1,88 @@
 package kdTree;
 
-public class KdTreeST {
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.Queue;
+
+import java.util.TreeMap;
+
+public class KdTreeST<Value> {
+    private TreeMap<Point2D, Value> tree;
+    
+    private class Node {
+    	   private Point2D p;      // the point
+    	   private Value value;    // the symbol table maps the point to this value
+    	   private RectHV rect;    // the axis-aligned rectangle corresponding to this node
+    	   private Node lb;        // the left/bottom subtree
+    	   private Node rt;        // the right/top subtree
+    }
+    
+    
+    // construct an empty symbol table of points
+    public KdTreeST() {
+        tree = new TreeMap<>();
+    }
+
+    // is the symbol table empty?
+    public boolean isEmpty() {
+        return tree.isEmpty();
+    }
+
+    // number of points
+    public int size() {
+        return tree.size();
+    }
+
+    //associate Value val with point p
+    public void put(Point2D p, Value val) {
+        if(p == null || val == null)
+            throw new NullPointerException("Arguments cannot be null");
+
+        tree.put(p, val);
+    }
+
+    //get Value associated with point p
+    public Value get(Point2D p) {
+        if(p == null)
+            throw new NullPointerException("Arguments cannot be null");
+
+        return tree.get(p);
+    }
+
+    //Does the symbol table contain point p?
+    public boolean contains(Point2D p) {
+        if(p == null)
+            throw new NullPointerException("Arguments cannot be null");
+
+        return tree.containsKey(p);
+    }
+
+    //Iterable of all points in the symbol table
+    public Iterable<Point2D> points() {
+        return tree.navigableKeySet();
+    }
+
+    //all points that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) {
+        Queue<Point2D> hits = new Queue<Point2D>();
+
+        for(Point2D point : tree.keySet())
+            if (rect.contains(point))
+                hits.enqueue(point);
+
+        return hits;
+    }
+
+    //a nearest neighbor to point p; null if the symbol table is empty
+    public Point2D nearest(Point2D p) {
+        if(p == null)
+            throw new NullPointerException("Arguments cannot be null");
+
+        Point2D nearest = tree.firstKey();
+
+        for(Point2D point : tree.keySet())
+            if(p.distanceSquaredTo(point) < p.distanceSquaredTo(nearest))
+                nearest = p;
+
+        return nearest;
+    }
 }
